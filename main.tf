4a112f580b3a65acc1c6871aa036dfd9eed55399
MissSB <missakiiki@gmail.com>
2025-05-03 14:47:35 -0400

    Update main

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
-  key_name               = "my-key" # Replace with your key
+  key_name               = "my-key" 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   subnet_id              = aws_subnet.main.id
 
